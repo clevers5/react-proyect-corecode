003ad33aa0e8460d9cff4cc4b86f3154eb4c096b
Clever <arnez.4292@gmail.com>
2022-10-04 20:32:39 -0400

    ProjectInitExpress

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+import express from "express";
 import { initDB } from "./db/index.js";
 
-initDB().then(() => {
-  console.log("DB Created :)");
+const Api = express();
+
+Api.use(express.json({}));
+Api.use(express.urlencoded({ extended: false }));
+
+Api.get("/test", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+Api.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  initDB().then(() => {
+    console.log("DB initialized");
+  });
 });
